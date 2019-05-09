f0a0f8cb826d201c7e0d8c1ff4d4f7b8d1f542a1
skworks33 <skworks33@gmail.com>
2019-05-09 16:30:13 +0900

    Updated to work on rails 5 (1st)

--- lib/custom_fields_helper_patch.rb
+++ lib/custom_fields_helper_patch.rb
@@ -2,34 +2,21 @@ require_dependency 'custom_fields_helper'
 
 module CustomFieldsHelperPatch
 
-  def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
+  def custom_field_label_tag(name, custom_value, options={})
 
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
-      alias_method_chain :custom_field_label_tag, :i18n
+    required = options[:required] || custom_value.custom_field.is_required?
+    custom_field_name = custom_value.custom_field.name
+    l_name = l(custom_field_name)
+    if l_name.index('translation missing:') == 0 && l_name.index(custom_field_name) == l_name.length - custom_field_name.length
+      l_name = custom_value.custom_field.description.presence
     end
+    content = content_tag 'span', l_name
 
-  end
-
-  module InstanceMethods
-
-    def custom_field_label_tag_with_i18n(name, custom_value, options={})
-      required = options[:required] || custom_value.custom_field.is_required?
-      custom_field_name = custom_value.custom_field.name
-      l_name = l(custom_field_name)
-      if l_name.index('translation missing:') == 0 && l_name.index(custom_field_name) == l_name.length - custom_field_name.length
-        l_name = custom_value.custom_field.description.presence
-      end
-      content = content_tag 'span', l_name
-
-      content_tag 'label', content +
-                             (required ? " <span class=\"required\">*</span>".html_safe : ''),
-                  :for => "#{name}_custom_field_values_#{custom_value.custom_field.id}"
-    end
-
+    content_tag 'label', content +
+                           (required ? " <span class=\"required\">*</span>".html_safe : ''),
+                :for => "#{name}_custom_field_values_#{custom_value.custom_field.id}"
   end
 
 end
 
-CustomFieldsHelper.send :include, CustomFieldsHelperPatch
+CustomFieldsHelper.prepend CustomFieldsHelperPatch
